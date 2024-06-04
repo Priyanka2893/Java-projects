d1cdd9e55037d29073e88222df8b73bb3fa69ac7
Priyanka Shaktawat <shaktawatp28@gmail.com>
2024-06-04 11:15:41 +0530

    logger and logback added

--- src/main/java/com/projects/EmployeeData/controller/EmployeeController.java
+++ src/main/java/com/projects/EmployeeData/controller/EmployeeController.java
@@ -2,6 +2,8 @@ package com.projects.EmployeeData.controller;
 
 import com.projects.EmployeeData.entity.Employee;
 import com.projects.EmployeeData.service.EmployeeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,10 +12,12 @@ import java.util.List;
 @ResponseBody
 @Controller
 public class EmployeeController {
+    private static final Logger logInfo = LoggerFactory.getLogger(EmployeeController.class);
     @Autowired
     private EmployeeService service;
     @GetMapping("/employees")
     public List<Employee> getEmployeeList(){
+        logInfo.info("Get employee list method is called");
         return service.getEmployeeList();
     }
     @GetMapping("/employee/{id}")
